df18f1ba0056345e57e217257a3bb595128381a5
myko98 <michaelluchen1998@gmail.com>
2023-11-10 19:12:20 -0500

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,63 @@ export class AppComponent {
   //   this.password = '';
   // }
   password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick() {
-    this.password = 'My password';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChar = '';
+
+    if (this.includeLetters) {
+      validChar += letters;
+    }
+    if (this.includeNumbers) {
+      validChar += numbers;
+    }
+    if (this.includeSymbols) {
+      validChar += symbols;
+    }
+
+    let counter = 0;
+    let pw = ''
+    while (counter < this.length) {
+      const index = Math.floor(Math.random() * validChar.length);
+      pw += validChar[index];
+      counter += 1;
+    }
+    this.password = pw;
   }
 
+  // getter function
   getPassword() {
-    return this.password
+    return this.password;
+  }
+
+  onChangeLength(value: string) {
+    const num = parseInt(value);
+    console.log(num)
+
+    if (!isNaN(num)) {
+      this.length = num;
+    } else {
+      this.length = 0;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
